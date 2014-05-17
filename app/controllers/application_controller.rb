edda53dfad68233e2c69cc8ef5511ea71b596895
Mike Stenzler <mike@nexuscafe.com>
2014-05-17 18:34:39 -0400

    Remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,40 +18,15 @@ class ApplicationController < ActionController::Base
   		CONFIG[enable_name]
   	end
   	helper_method enable_name
-#  	p "defined method: '#{enable_name}'"
   	define_method require_name do
   		CONFIG[require_name]
   	end
   	helper_method require_name
-#  	p "defined method: '#{require_name}'"
   	define_method use_name do |user_object|
   		user_object.new_record? ? CONFIG[require_name] : CONFIG[enable_name]
   	end
   	helper_method use_name
-#  	p "defined method: '#{use_name}'"
   end
-
-=begin
-  def enable_name?
-  	CONFIG[:enable_name?]
-  end
-  helper_method :enable_name?
-
-  def enable_username?
-  	CONFIG[:enable_username?]
-  end
-  helper_method :enable_username?
-
-  def enable_gender?
-  	CONFIG[:enable_gender?]
-  end
-  helper_method :enable_gender?
-
-  def enable_birthdate?
-  	CONFIG[:enable_birthdate?]
-  end
-  helper_method :enable_birthdate?
-=end
 
   def signed_in_user
   	unless signed_in?
@@ -62,6 +37,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+    #enables each individual user to use a specific time zone if selected
     def user_time_zone(&block)
       Time.use_zone(current_user.time_zone, &block) if !current_user.time_zone.blank?
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 SampleApp::Application.routes.draw do
 
-#  get "username_changes/new"
-#  get "password_resets/new"
   resources :password_resets
   resources :users do
     resources :verify_emails, only: [:new, :create], shallow: true
     resources :change_usernames, only: [:edit, :update], shallow: true
     resources :change_emails, only: [:edit, :update], shallow: true
-#    member do
-#      get 'verify_email_token/:verify_token', to: "verify_emails#create", as: 'verify_email_token'
-#    end
-#    resources :username_changes, shallow: false
-#    member do
-#      get 'verify_email/:verify_token', to: "users#verify_email"
-#      get 'verify_email'
-#      patch 'verify_email'
-#    end
   end
 
-  get '/users/:user_id/verify_email_token/:verify_token', to: "verify_emails#create", as: 'verify_email_token'
+  get '/users/:user_id/verify_email_token/:verify_token', 
+       to: "verify_emails#create", as: 'verify_email_token'
  
   resources :sessions, only: [:new, :create, :destroy]
 
@@ -31,58 +21,5 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
